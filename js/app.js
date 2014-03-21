4a80a198d2bb5e14a4ca043b987a6af55f00bcc5
Mohamed Farazi <mohamed.farazi@gmail.com>
2014-03-21 16:40:23 -0400

    added favo, RTs to tweets

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+	this.resource('about');
 	this.resource('tweets', function(){
 		this.resource('tweet', {path: ':tweet_id'});
 	});
-	this.resource('about');
+
 })
 
 App.TweetsRoute = Ember.Route.extend({
@@ -19,33 +20,43 @@ var tweets = [{
 	user: 'Mo',
 	date: new Date('12-12-2013'),
 	body: "Check it out, I'm doing this tweeting thing that's the hype now",
-	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png'
+	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png',
+	rt: '3',
+	fav: '1'
 },
 {
 	id: '2',
 	user: 'Mo',
 	date: new Date('01-01-2014'),
 	body: "amagaaad guiiiseee, it's the new year!!!111one!!",
-	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png'
+	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png',
+	rt: '3',
+	fav: '1'
 },
 {
 	id: '3',
 	user: 'Mo',
 	date: new Date('12-12-2013'),
 	body: "Check it out, I'm doing this tweeting thing that's the hype now",
-	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png'
+	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png',
+	rt: '3',
+	fav: '1'
 },
 {
 	id: '4',
 	user: 'Mo',
 	date: new Date('12-12-2013'),
 	body: "Check it out, I'm doing this tweeting thing that's the hype now",
-	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png'
+	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png',
+	rt: '3',
+	fav: '1'
 },
 {
 	id: '5',
 	user: 'Mo',
 	date: new Date('12-12-2013'),
 	body: "Check it out, I'm doing this tweeting thing that's the hype now",
-	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png'
+	pic: 'http://soapboxhq.com/wp-content/themes/hitsend/img/soapbox_logo.png',
+	rt: '3',
+	fav: '1'
 }];
